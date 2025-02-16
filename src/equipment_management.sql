8107c4cf0b1ad7dc62951bc7fa6b647c06fbf99c
qaisa076 <qais.ahmad@ada.ac.uk>
2025-02-16 18:06:45 +0000

    equipment management

--- src/equipment_management.sql
+++ src/equipment_management.sql
@@ -3,14 +3,40 @@
 .mode column
 
 -- Enable foreign key support
+PRAGMA FOREIGN_KEY = ON;
 
 -- Equipment Management Queries
 
 -- 1. Find equipment due for maintenance
 -- TODO: Write a query to find equipment due for maintenance
+SELECT
+    equipment_id,
+    name,
+    type,
+    next_maintenance_date
+FROM
+    equipment
+WHERE
+    next_maintenance_date <= Date('now');
+
 
 -- 2. Count equipment types in stock
 -- TODO: Write a query to count equipment types in stock
+SELECT 
+    type,
+    COUNT(*) AS equipment_count
+FROM 
+    equipment
+GROUP BY 
+    type;
+
 
 -- 3. Calculate average age of equipment by type (in days)
 -- TODO: Write a query to calculate average age of equipment by type (in days)
+SELECT
+    e.type,
+    AVG(JULIANDAY('now') - JULIANDAY(e.purchase_date)) AS average_age_days
+FROM
+    equipment AS e
+GROUP BY
+    e.type;
